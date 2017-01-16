19e9e1b4321b655dddae2d12a31bf758e191ef5d
Mike McCabe <mike.mccabe@bodykind.com>
2017-01-16 14:48:39 +0000

    Use fs to load package.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
+const fs = require('fs');
 
 module.exports = function(source) {
   this.cacheable();
-  const packageJSON = require('./package.json');
+  const packageJSON = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
   var manifest = JSON.parse(source);
   manifest.version = packageJSON.version;
   return JSON.stringify(manifest, null, '\t');
