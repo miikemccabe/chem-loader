6c618fc76c5da24a1cd7226a8b893a83d745bb8b
Mike McCabe <mike.mccabe@bodykind.com>
2017-01-24 15:59:15 +0000

    Add test for unknown option

--- test.js
+++ test.js
@@ -13,14 +13,15 @@ const webpackContext = {
 const manifestLoader = loader.bind(webpackContext);
 
 describe('chrome-manifest-loader', function () {
+
   it('Manifest version should match package.json', function () {
     const parsedManifest = manifestLoader(JSON.stringify(manifest));
     const given = JSON.parse(parsedManifest).version;
     const expected = packageJSON.version;
     assert.equal(given, expected);
   });
-  it('Manifest name should match package.json', function () {
 
+  it('Manifest name should match package.json', function () {
     const parsedManifest = loader.call(
       Object.assign({}, webpackContext, {
         query: '?useName=true'
@@ -30,8 +31,8 @@ describe('chrome-manifest-loader', function () {
     const expected = packageJSON.name;
     assert.equal(given, expected);
   });
-  it('Manifest description should match package.json', function () {
 
+  it('Manifest description should match package.json', function () {
     const parsedManifest = loader.call(
       Object.assign({}, webpackContext, {
         query: '?useDescription=true'
@@ -41,8 +42,8 @@ describe('chrome-manifest-loader', function () {
     const expected = packageJSON.description;
     assert.equal(given, expected);
   });
-  it('Manifest author should match package.json', function () {
 
+  it('Manifest author should match package.json', function () {
     const parsedManifest = loader.call(
       Object.assign({}, webpackContext, {
         query: '?useAuthor=true'
@@ -50,6 +51,17 @@ describe('chrome-manifest-loader', function () {
     );
     const given = JSON.parse(parsedManifest).author;
     const expected = packageJSON.author;
+    assert.equal(given, expected);
+  });
+
+  it('Should still work if an unknown option is supplied', function () {
+    const parsedManifest = loader.call(
+      Object.assign({}, webpackContext, {
+        query: '?useJibberish=true'
+      }), JSON.stringify(manifest)
+    );
+    const given = JSON.parse(parsedManifest).version;
+    const expected = packageJSON.version;
     assert.equal(given, expected);
   });
 });
